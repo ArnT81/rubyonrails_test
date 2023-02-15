25e0dcb34d667c0df2a8edb2c5e40d879eb4789b
ArnT81 <anders_soderberg@hotmail.com>
2023-02-15 09:29:36 +0100

    added my first route (about)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  resources :bubbles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #!root is different
+
+get '/about' => 'about#index'
+
+
 
   # Defines the root path route ("/")
   # root "articles#index"
