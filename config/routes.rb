f0d7d55933527769cd6b050f7336b08b17662674
ArnT81 <anders_soderberg@hotmail.com>
2023-02-15 15:57:05 +0100

    added table Cats

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :bubbles
+  resources :cats
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   #!root is different
 
-get '/about' => 'about#index'
+  get '/about' => 'about#index'
 
 
 
