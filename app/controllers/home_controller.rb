8f2f8437911395ceae16301d15a407e4de35f375
ArnT81 <anders_soderberg@hotmail.com>
2023-02-19 11:37:02 +0100

    conditional login/logout links with CurrentAttributes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,4 @@
 class HomeController < ApplicationController
   def index
-    if session[:user_id]
-      @user = User.find_by(id: session[:user_id]) #app crash on find() if user is removed
-    end
-    #flash[:notice] = "Succesfully logged in"
-    #flash[:alert] = "Invalid email or password"
   end
 end
